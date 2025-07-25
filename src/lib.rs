40487329dac239a32674c50aaa7364a5077e703c
kilic <onurkilic1004@gmail.com>
2025-07-25 17:40:27 +0300

    init

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+#[derive(Debug, Clone, PartialEq, Eq, Copy)]
+pub enum Error {
+    Transcript,
+    Verify,
+}
+
+#[cfg(test)]
+pub(crate) mod test {
+    use rand::{rngs::StdRng, SeedableRng};
+
+    #[allow(dead_code)]
+    pub(crate) fn seed_rng() -> StdRng {
+        StdRng::seed_from_u64(1)
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn init_tracing() {
+        use tracing_forest::util::LevelFilter;
+        use tracing_forest::ForestLayer;
+        use tracing_subscriber::layer::SubscriberExt;
+        use tracing_subscriber::util::SubscriberInitExt;
+        use tracing_subscriber::{EnvFilter, Registry};
+
+        let env_filter = EnvFilter::builder()
+            .with_default_directive(LevelFilter::INFO.into())
+            .from_env_lossy();
+
+        Registry::default()
+            .with(env_filter)
+            .with(ForestLayer::default())
+            .init();
+    }
+}
